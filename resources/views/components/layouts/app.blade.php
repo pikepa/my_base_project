ba53e9180074effc2aca7a48762d533d28298b09
Peter Pike <pikepeter@gmail.com>
2025-06-06 15:43:05 +0400

    initial commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? config('app.name') }}</title>
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="bg-sky-100">
+        <div class="bg-red-50 max-w-5xl mx-auto my-12 p-6">
+            {{ $slot }}
+        </div>
+    </body>
+</html>
---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,3 @@
+<div class="font-bold text-4xl text-center">
+    {{config('app.name')}}
+</div>
