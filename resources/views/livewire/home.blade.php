f07ff2146b262d6eeb4c6bca8130c961026b28bf
Peter Pike <pikepeter@gmail.com>
2025-06-07 19:39:09 +0400

    update to home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,6 +3,15 @@
         {{config('app.name')}}
     </div>
     <div class="mt-6 text-xl text-center">
-<p>This is a project which has been created using Laravel Livewire, a powerful framework for building dynamic web applications.</p>
+        <p>This is a project which has been created using:</p>
+    <div class="mx-auto max-w-60 mt-4">
+        <ul class="list-disc marker:text-sky-400 text-left  ">
+            <li>The Laravel Framework</li>
+            <li>Laravel Livewire</li>
+            <li>Tailwind CSS</li>
+            <li>Pest</li>
+        </ul>
+    </div>
+
     </div>
 </div>
