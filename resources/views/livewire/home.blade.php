b10efaff39472c1267a817ec36c10364fe8153d9
Peter Pike <pikepeter@gmail.com>
2025-06-07 17:18:51 +0400

    Updated packag-lock and the home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,8 @@
-<div class="font-bold text-4xl text-center">
-    {{config('app.name')}}
+<div>
+    <div class="font-bold text-4xl text-center">
+        {{config('app.name')}}
+    </div>
+    <div class="mt-6 text-xl text-center">
+<p>This is a project which has been created using Laravel Livewire, a powerful framework for building dynamic web applications.</p>
+    </div>
 </div>
